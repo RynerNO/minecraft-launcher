01d184dadfc0aaf2d34294da28bdda8197d5cbdd
RynerNO <ryner.no@gmail.com>
2021-04-04 00:44:53 +0300

    removed unchanged css

--- src/renderer/app.ts
+++ src/renderer/app.ts
@@ -2,17 +2,17 @@ import { createApp } from 'vue';
 import { router } from './router';
 import { store } from './store';
 import PrimeVue from 'primevue/config';
-import App from "./App.vue";
-import './assets/themes/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css';    
-const app = createApp(App)
-app.use(router)
-app.use(store)
-app.use(PrimeVue)
+import App from './App.vue';
+import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
+import './assets/themes/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(PrimeVue);
 
-app.provide('ipcRenderer', window.ipcRenderer)
+app.provide('ipcRenderer', window.ipcRenderer);
 
 app.mount('#app');
-
