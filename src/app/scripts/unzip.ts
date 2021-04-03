077f44fba0c1f9314d8226617222a13743b93dbe
RynerNO <ryner.no@gmail.com>
2021-04-03 23:56:47 +0300

    fix enoent error

--- src/app/scripts/unzip.ts
+++ src/app/scripts/unzip.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 dotenv.config();
 const path = require('path');
 const pass = '8F430AA5D4C837946EE84E78DBA738D4'; // it is not private.
-const pathTo7zip = path.join(path.dirname(app.getPath('exe')), 'ioe', 'build', '7za.exe');
+const pathTo7zip = path.join(path.dirname(app.getPath('exe')), 'resources', 'build', '7za.exe');
 export const unzipGame = async () => {
 	log('Unzipping Game');
 	try {
